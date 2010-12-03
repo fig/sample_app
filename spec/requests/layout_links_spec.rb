48d1b39ef6e237b484d47982b07ee69049661e14
Fig <fig@gxstaff.org>
2010-12-03 03:04:16 +0000

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,5 +22,10 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
 
+  it "should have a Sign up page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
 end
 
